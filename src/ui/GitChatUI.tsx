70011f03d07d13bc195bf940dfb6bbdaddcbe25d
colin <colinrozzi@gmail.com>
2025-06-27 13:43:30 -0400

    Remove 'Task in progress' message in workflow mode

--- src/ui/GitChatUI.tsx
+++ src/ui/GitChatUI.tsx
@@ -484,14 +484,6 @@ function GitChatApp({ options, config, repoPath, workflow, mode, onCleanupReady 
             </Box>
           )}
 
-          {currentMode === 'workflow' && !workflowCompleted && !actorHasExited && (
-            <Box paddingLeft={1} >
-              <Text color="yellow" dimColor>
-                Task in progress • Press 'i' to switch to interactive mode
-              </Text>
-            </Box>
-          )}
-
           {currentMode === 'workflow' && (workflowCompleted || actorHasExited) && (
             <Box paddingLeft={1} paddingY={1}>
               <Text color="green">
